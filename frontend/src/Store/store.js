8f4cd540e20606be89525453e11c0927f9747042
Harish M <harinani2416@gmail.com>
2024-03-18 10:46:52 +0530

    Implemented property-details and login page and payment gateway

--- frontend/src/Store/store.js
+++ frontend/src/Store/store.js
@@ -1,9 +1,14 @@
-import {configureStore} from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import propertySlice from "./Property/property-slice";
+import propertyDetailsSlice from "./PropertyDetails/propertyDetails-slice";
+import userSlice from "./User/user-slice";
 
-const store =configureStore({
-    reducer:{
-        properties:propertySlice.reducer,
-    },
+const store = configureStore({
+  reducer: {
+    properties: propertySlice.reducer,
+    propertydetails: propertyDetailsSlice.reducer,
+    user:userSlice.reducer,
+  },
 });
+
 export default store;
